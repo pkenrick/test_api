ec49d6b49bff95b644e9196d87ed1b109cf59620
paulkenrick <pkenrick@gmail.com>
2017-10-01 16:24:51 +0100

    Test

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -22,6 +22,7 @@ class Api::BaseController < ApplicationController
 
   def authenticate_token
     puts "====== in the authenticate token method ======="
+    puts request.headers
     authenticate_with_http_token do |token, options|
       puts "==== #{token}"
       puts "---- #{options}"
